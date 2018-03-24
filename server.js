ae5ad2ef892d00753dab021497946f8ded51be8b
Lawrence Anderson <anderson0741@icloud.com>
2018-03-23 18:05:26 -0600

    initial launch

--- server.js
+++ server.js
@@ -5,20 +5,30 @@ const expressJwt = require("express-jwt");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require("morgan");
+const path = require("path");
+const port = process.env.PORT || 8088;
+const secret = process.env.SECRET || "Millyswede Lawrence Schnorkavich arche duke"  
 
-mongoose.connect("mongodb://localhost/listings", (err) => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/listings", (err) => {  
+// mongoose.connect("mongodb://localhost/listings", (err) => {
+// mongoose.connect("/listings", (err) => {
     if (err) throw err;
     console.log("Connected to the Listings Database");
 });
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 // app.use("/api/public/listing", require('./routes/public'));
 app.use("/api/listing/", expressJwt({secret: process.env.SECRET}));
 app.use("/auth", require("./routes/auth"));
 app.use('/listing', require('./routes/routes'));
 app.use('/api/upload', require('./routes/routes'));
 
-app.listen(8088, () => {
-    console.log("Server is running on port 8088");
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
