18909e06b099fc7e135458c6a5d9230eab04a2a3
Lawrence Anderson <anderson0741@icloud.com>
2018-03-26 15:46:04 -0600

    mobile

--- client/src/shared/Home/index.js
+++ client/src/shared/Home/index.js
@@ -7,19 +7,23 @@ function Home(props) {
             <div className='homeDivBox'>
                 <br />
                 <div className="welcome_level">
-                    <h1 className='tag zoomIn'>Welcome to Level Up Cars</h1>
+                    <h1 className='tag'>Welcome to Level Up Cars</h1>
+                </div>
+                <div className="welcome_level_mobile">
+                    <h2 className='tag'>Welcome to</h2>
+                    <h1>Level Up Cars</h1>
                 </div>
                 <div className="selector">
-                    <a href="cars" className="shopCars homeBox">
+                    <a href="cars" className="shopCars cons1">
                         <h1>SHOP CARS</h1>
                     </a>
-                    <a className="aboutSelector homeBox" href="/about">
+                    <a className="aboutSelector cons1" href="/about">
                         <h1>ABOUT US</h1>
                     </a>
-                    <a href="contact" className="location homeBox">
+                    <a href="contact" className="location consTitle">
                         <h1>LOCATION</h1>
                     </a>
-                    {/* <div className="otherFuture homeBox">
+                    {/* <div className="otherFuture cons1">
                         <h1>future</h1>
                     </div> */}
                 </div>
